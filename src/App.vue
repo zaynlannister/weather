f62ff50fae1842f6631b11f9ebc31274e0eb4798
zaynlannister <zaynlannister@gmail.com>
2022-12-06 18:11:53 +0500

    add app-weather component

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,26 @@
   <div class="header">
     <div class="container">
       <img class="header-logo" src="@/assets/weather-logo.png">
-      <search-form
-        v-model="cityName"
-      />
+      <search-form v-model="cityName"/>
     </div>
   </div>
-  <div class="container">
-    <div class="weather">
-      <div class="weather-date">Dec 6, 08:24am</div>
-      <div class="weather-city">London</div>
-      <div class="weather-temp">
-        <img src="@/assets/clouds.svg">
-        <span>3°C</span>
-      </div>
-      <div class="weather-info">Fells like 2°C. Broken clouds. Light air</div>
-    </div>
-  </div>
+  <app-weather :data="weatherDate"/>
 </template>
 
 <script>
 import SearchForm from "@/components/SearchForm.vue";
+import AppWeather from "@/components/AppWeather.vue";
 
 export default {
   components: {
     SearchForm,
+    AppWeather
   },
 
   data() {
     return {
-      cityName: ""
+      cityName: "",
+      weatherDate: []
     }
   }
 }
@@ -57,36 +48,5 @@ export default {
     align-items: center;
     justify-content: center;
     padding: 20px 0;
-  }
-
-  .weather {
-    padding-top: 20px;
-  }
-
-  .weather-date {
-    color: #eb6e4b;
-  }
-
-  .weather-city {
-    font-size: 24px;
-    font-weight: bold;
-  }
-
-  .weather-temp {
-    display: flex;
-    align-items: center;
-    font-size: 30px;
-    font-weight: 500;
-    margin: 10px 0 10px;
-  }
-
-  .weather-temp img {
-    width: 40px;
-    height: 40px;
-    margin-right: 8px;
-  }
-
-  .weather-info {
-    font-weight: 500;
   }
 </style>
---
+++ src/components/AppWeather.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="weather">
+      <div class="weather-date">Dec 6, 08:24am</div>
+      <div class="weather-city">London</div>
+      <div class="weather-temp">
+        <img src="@/assets/clouds.svg">
+        <span>3°C</span>
+      </div>
+      <div class="weather-info">Fells like 2°C. Broken clouds. Light air</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    weatherDate: Object
+  }
+}
+</script>
+
+<style scoped>
+  .weather {
+    padding-top: 20px;
+  }
+
+  .weather-date {
+    color: #eb6e4b;
+  }
+
+  .weather-city {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  .weather-temp {
+    display: flex;
+    align-items: center;
+    font-size: 30px;
+    font-weight: 500;
+    margin: 10px 0 10px;
+  }
+
+  .weather-temp img {
+    width: 40px;
+    height: 40px;
+    margin-right: 8px;
+  }
+
+  .weather-info {
+    font-weight: 500;
+  }
+</style>
