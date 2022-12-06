3f1e4a118d1e2083915b1659b5ec1cadba877d1a
zaynlannister <zaynlannister@gmail.com>
2022-12-06 14:13:53 +0500

    add weather section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="header">
-      <div class="container">
-        <img class="header-logo" src="@/assets/weather-logo.png">
-        <div>
-          <input class="input" type="text" placeholder="Search city">
-          <button class="button">Search</button>
-        </div>
+  <div class="header">
+    <div class="container">
+      <img class="header-logo" src="@/assets/weather-logo.png">
+      <div>
+        <input class="input" type="text" placeholder="Search city">
+        <button class="button">Search</button>
       </div>
     </div>
+  </div>
+  <div class="container">
+    <div class="weather">
+      <div class="weather-date">Dec 6, 08:24am</div>
+      <div class="weather-city">London</div>
+      <div class="weather-temp">
+        <img src="@/assets/clouds.svg">
+        <span>3°C</span>
+      </div>
+      <div class="weather-info">Fells like 2°C. Broken clouds. Light air</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,7 +31,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
   .container {
     width: 1080px;
     margin: 0 auto;
@@ -71,5 +82,36 @@ export default {
     align-items: center;
     justify-content: center;
     padding: 20px 0;
+  }
+
+  .weather {
+    padding-top: 20px;
+  }
+
+  .weather-date {
+    color: #eb6e4b;
+  }
+
+  .weather-city {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  .weather-temp {
+    display: flex;
+    align-items: center;
+    font-size: 30px;
+    font-weight: 500;
+    margin: 10px 0 10px;
+  }
+
+  .weather-temp img {
+    width: 40px;
+    height: 40px;
+    margin-right: 8px;
+  }
+
+  .weather-info {
+    font-weight: 500;
   }
 </style>
