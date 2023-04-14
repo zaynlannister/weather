1a943f5c29aef09e722fc39579baf10401180e7d
zaynlannister <zaynlannister@gmail.com>
2023-04-14 21:08:55 +0500

    fix: remove loading if not found

--- src/App.vue
+++ src/App.vue
@@ -63,6 +63,7 @@ export default {
                   this.isLoading = false;
                 } else {
                   alert("Город не найден");
+                  this.isLoading = false;
                 }
               })
         } catch (err) {
